bda474579cf6a5000d897a7da264b8714e2bcc06
Alexander Kalinin <Kpojlukkreativnyj@gmail.com>
2024-01-08 19:35:04 +0100

    Exam was updated.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +11,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Effects;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
--- Presentation/Pages/Admin/CRUD Teacher/EditTeacherPage.xaml.cs
+++ Presentation/Pages/Admin/CRUD Teacher/EditTeacherPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Data;
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
